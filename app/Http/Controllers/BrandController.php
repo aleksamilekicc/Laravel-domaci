cf4d58d362e550067ebe92cc16b5bd904f1ee8f6
Aleksa Milekic <aleksamilekic@gmail.com>
2024-08-16 18:43:30 +0200

    brandcontr

---
+++ app/Http/Controllers/BrandController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Brand;
+use Illuminate\Http\Request;
+
+class BrandController extends Controller
+{
+    public function index()
+    {
+        $brands = Brand::all();
+        return response()->json(['brands' => $brands], 200);
+    }
+}
